5ad7eab8fdba8591faa2ccf681ec4c8cbbf5e881
amartyaa <amartyanshuman.thakur@gmail.com>
2020-11-09 22:08:27 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,47 @@
+# InShorts API
+
+News Article Publishing API made in GoLang.
+ 
+
+
+## Installation
+
+Use the [GIT](https://git-scm.com/downloads) to download this API.
+
+```bash
+git clone https://github.com/amartyaa/AppointyTask
+```
+
+## Pre-requisites
+These packages must be downloaded/installed for this code to run successfully
+```bash
+go get go.mongodb.org/mongo-driver
+go get github.com/julienschmidt/httprouter
+```
+
+## Usage
+Make sure you have MongoDB servers running.
+
+```Go
+go run server.go
+```
+
+
+
+## Comments
+
+Third-Party Packages were not allowed thus not using Gorilla MUX. Though Still using httprouter from [julienschmidt](https://github.com/julienschmidt/httprouter).
+
+## References
+With no knowledge of GoLang two days ago, this code was made in less than 48 hours. Thanks to these links from where I learned and implemented codes to make this task happen in time.
+
+[Mongo<->Go connection](https://www.mongodb.com/golang)
+
+[GitBook provided with the task for reference](https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/)
+
+Many more websites and articles came to help, can't mention all of them.
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+### _Recruitment task for Appointy_
+[Task can be found at this link](https://docs.google.com/document/d/1puAfEgm47CjUUE6ivtyDnYbBFRk6NH9hWcbtMmJowPw/edit#)
---
+++ server.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+//Article structure
+type Article struct {
+	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title string             `json:"title" bson:"title,omitempty"`
+	Body  string             `json:"body" bson:"body,omitempty"`
+	Tags  string             `json:"tags" bson:"tags,omitempty"`
+}
+
+//Creating collection var as global variable for easy access across Functions
+var collection = ConnecttoDB()
+
+func main() {
+	//Init Router
+	router := httprouter.New()
+
+	//http routing
+	router.GET("/article", getArticles)
+	router.GET("/article/:id", getArticle)
+	router.GET("/articles/search?q=title", searchArticle)
+	router.POST("/articles", createArticle)
+	// set our port address as 8081
+	log.Fatal(http.ListenAndServe(":8081", router))
+}
+
+// ConnecttoDB : unction to connect to mongoDB locally
+func ConnecttoDB() *mongo.Collection {
+
+	// Set client options
+	//change the URI according to your database
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	//Error Handling
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	//DB collection address which we are going to use
+	//available to functions of all scope
+	collection := client.Database("Appointy").Collection("NewsArticles")
+
+	return collection
+}
+
+//Function to get all Articles in DataBase
+func getArticles(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Article array
+	var Articles []Article
+
+	// bson.M{},  we passed empty filter of unordered map.
+	cur, err := collection.Find(context.TODO(), bson.M{})
+
+	//Error Handling
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the cursor once finished
+	defer cur.Close(context.TODO())
+
+	//Loops over the cursor stream and appends to []Article array
+	for cur.Next(context.TODO()) {
+
+		// create a value into which the single document can be decoded
+		var article Article
+		// decode similar to deserialize process.
+		err := cur.Decode(&article)
+
+		//Error Handling
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// add item our array
+		Articles = append(Articles, article)
+	}
+	//Error Handling
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	//Encoding the data in Array to JSON format
+	json.NewEncoder(w).Encode(Articles)
+}
+
+//Function to create a new Article in Database
+func createArticle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var article Article
+
+	//Decoding the Data from JSON format to Article variable
+	_ = json.NewDecoder(r.Body).Decode(&article)
+	//inserts the data from decoded var to MongoDB in BSON format
+	result, err := collection.InsertOne(context.TODO(), article)
+	//Error Handling
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(result)
+}
+
+//Function to search Article by ID
+func getArticle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var article Article
+
+	// string to primitive.ObjectID (typeCasting)
+	id, _ := primitive.ObjectIDFromHex(ps.ByName("id"))
+
+	// creating filter of unordered map with ID as input
+	filter := bson.M{"_id": id}
+
+	//Searching in DB with given ID as keyword
+	err := collection.FindOne(context.TODO(), filter).Decode(&article)
+	//Error Handling
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(article)
+}
+
+//Function to search Article by title
+func searchArticle(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var article Article
+
+	//recovers the argument of search query present in URL after "q"
+	title := string(r.URL.Query().Get("q"))
+
+	//makes an unordered map filter of title
+	filter := bson.M{"title": title}
+
+	//Searching in DB with given title as keyword
+	err := collection.FindOne(context.TODO(), filter).Decode(&article)
+	//Error Handling
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(article)
+}
